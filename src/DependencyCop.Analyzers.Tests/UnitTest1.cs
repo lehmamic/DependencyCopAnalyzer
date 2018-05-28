eb94e1e23f4b2f68fa7220924ae9153887aaa0ee
Michael Lehmann <lehmamic@bluewin.ch>
2018-05-28 11:41:58 +0200

    Add the unit test project

---
+++ src/DependencyCop.Analyzers.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DependencyCop.Analyzers.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
